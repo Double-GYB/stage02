38851a9f2e9e16e4dea05b749068d79681d9f95a
Levi <lvze@tedu.cn>
2020-02-12 17:17:43 +0800

    d7

--- day07/chat_client.py
+++ day07/chat_client.py
@@ -4,10 +4,33 @@ chat room 客户端
 """
 
 from socket import *
-import os
+import os,sys
 
 # 服务端地址
 ADDR = ('127.0.0.1',8888)
+
+# 发送消息
+def send_msg(s,name):
+    while True:
+        try:
+            content = input("发言:")
+        except:
+            content = 'quit'
+        if content == 'quit':
+            msg = "Q " + name
+            s.sendto(msg.encode(),ADDR)
+            sys.exit("谢谢使用")
+        msg = "C %s %s"%(name,content) # 根据协议组织消息
+        s.sendto(msg.encode(),ADDR)
+
+# 接收消息
+def recv_msg(s):
+    while True:
+        data,addr = s.recvfrom(1024)
+        # 该进程应该退出
+        if data.decode() == "EXIT":
+            sys.exit()
+        print(data.decode()+'\n发言:',end="")
 
 # 启动函数 -->  向服务端发送初始请求
 def main():
@@ -25,6 +48,15 @@ def main():
         else:
             print("该用户已存在")
 
+    # 创建父子进程
+    pid = os.fork()
+    if pid < 0:
+        print("error")
+        return
+    elif pid == 0:
+        send_msg(s,name)
+    else:
+        recv_msg(s)
 
 if __name__ == '__main__':
     main()
