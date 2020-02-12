d787c86022763aaac43215640688a7d81fd1cc43
Levi <lvze@tedu.cn>
2020-02-12 15:46:42 +0800

    d7

--- day07/chat_client.py
+++ day07/chat_client.py
@@ -14,7 +14,17 @@ def main():
     # udp 客户端
     s = socket(AF_INET,SOCK_DGRAM)
 
-    s.sendto(b"request",ADDR)
+    while True:
+        name = input("请输入姓名:")
+        msg = "L "+name  # 组织协议的格式
+        s.sendto(msg.encode(),ADDR) # 发送请求
+        data,addr = s.recvfrom(128) # 等待反馈结果
+        if data.decode() == "OK":
+            print("您已进入聊天室")
+            break
+        else:
+            print("该用户已存在")
+
 
 if __name__ == '__main__':
     main()
--- day07/chat_server.py
+++ day07/chat_server.py
@@ -9,6 +9,22 @@ from socket import *
 # 全局变量
 ADDR = ('0.0.0.0',8888)
 
+# 应用于存储用户 {name:address}
+user={}
+
+# 处理进入聊天室请求
+def do_login(s,name,addr):
+    if name in user:
+        s.sendto(b"FAIL",addr)
+        return
+    else:
+        s.sendto(b"OK", addr)
+
+    # 通知其他人
+    msg = "欢迎 '%s' 进入聊天室"%name
+    for i in user:
+        s.sendto(msg.encode(),user[i])
+    user[name] = addr # 字典中加入一项
 
 # 基本结构 （接收请求，分配任务）
 def main():
@@ -19,11 +35,14 @@ def main():
     # 循环接收请求
     while True:
         data,addr = s.recvfrom(1024)
-        print("接收到的请求:",data.decode())
+        tmp = data.decode().split(' ') # 请求内容做简单的解析
+        print("接收到的请求:", tmp)
         # data --> 接收到的请求
-        if data == 'jr':
+        if tmp[0] == 'L':
+            do_login(s,tmp[1],addr)
+        elif tmp[0] == 'C':
             pass
-        elif data == 'lt':
+        elif tmp[0] == 'Q':
             pass
 
 if __name__ == '__main__':
