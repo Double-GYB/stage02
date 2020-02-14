fb4022c9c7193986d18da504c57ac9a2346ea0e2
Levi <lvze@tedu.cn>
2020-02-14 14:54:08 +0800

    d8

--- day08/exercise_2.py
+++ day08/exercise_2.py
@@ -8,6 +8,7 @@
 """
 
 import time
+from multiprocessing import Process
 
 def timeit(f):
     def wrapper(*args,**kwargs):
@@ -17,3 +18,60 @@ def timeit(f):
         print("函数执行时间：",end_time-start_time)
         return res
     return  wrapper
+
+
+# 判断一个数是不是质数
+def isPrime(n):
+    if n <= 1:
+        return False
+    for i in range(2,n):
+        if n % i == 0:
+           return False
+    return True
+
+@timeit
+def no_process():
+    prime = []
+    for i in range(1,100001):
+        if isPrime(i):
+            prime.append(i)
+    print(sum(prime))
+
+# no_process()  # 函数执行时间： 30.525405168533325
+
+class Prime(Process):
+    def __init__(self,begin,end):
+        super().__init__()
+        self.begin = begin
+        self.end = end
+
+    def run(self):
+        prime = []
+        for i in range(self.begin, self.end):
+            if isPrime(i):
+                prime.append(i)
+        print(sum(prime))
+
+@timeit
+def process_4():
+    jobs = []
+    for i in range(1,100001,25000):
+        p = Prime(i,i+25000)
+        jobs.append(p)
+        p.start()
+    for i in jobs:
+        i.join()
+
+# process_4()  # 函数执行时间： 19.373581886291504
+
+@timeit
+def process_10():
+    jobs = []
+    for i in range(1,100001,10000):
+        p = Prime(i,i+10000)
+        jobs.append(p)
+        p.start()
+    for i in jobs:
+        i.join()
+
+process_10() # 函数执行时间： 19.56751275062561
