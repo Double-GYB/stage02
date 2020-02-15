f8871153dbc64c388908fbee446b22218ad078d1
Levi <lvze@tedu.cn>
2020-02-15 14:21:11 +0800

    d9

--- day09/exercise_2.py
+++ day09/exercise_2.py
@@ -4,3 +4,31 @@
 
 提示： 可能用多个同步互斥对象
 """
+from threading import Thread,Lock
+
+lock1 = Lock()
+lock2 = Lock()
+
+def print_num():
+    for i in range(1,53,2):
+        lock1.acquire()
+        print(i)
+        print(i + 1)
+        lock2.release()
+
+def print_char():
+    for i in range(65,91):
+        lock2.acquire()
+        print(chr(i)) # 打印字母
+        lock1.release()
+
+t1 = Thread(target=print_num)
+t2 = Thread(target=print_char)
+
+lock2.acquire() # 让 print_num 函数先打印
+
+
+t1.start()
+t2.start()
+t1.join()
+t2.join()
