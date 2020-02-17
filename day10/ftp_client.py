e99b404b71f6031d4f8be790222c41f5aebe1c64
Levi <lvze@tedu.cn>
2020-02-17 15:06:25 +0800

    d10

--- day10/ftp_client.py
+++ day10/ftp_client.py
@@ -2,10 +2,10 @@
 ftp 客户端
 """
 from socket import *
-import sys
+import sys,time
 
 # 全局变量
-ADDR = ('127.0.0.1',8888)
+ADDR = ('127.0.0.1',8800)
 
 # 查看文件列表，上传，下载，退出
 class FTPClient:
@@ -29,6 +29,49 @@ class FTPClient:
         else:
             print(data)  # 打印原因
 
+    # 下载文件
+    def get(self,filename):
+        data = "G "+filename
+        self.sockfd.send(data.encode()) # 发送请求
+        # 等待回复
+        data = self.sockfd.recv(128).decode()
+        if data == 'OK':
+            # 接收文件
+            f = open(filename,'wb')
+            # 循环接收文件写入本地
+            while True:
+                data = self.sockfd.recv(1024)
+                # 文件接收完毕的标志
+                if data == b'##':
+                    break
+                f.write(data)
+            f.close()
+
+        else:
+            print(data)
+
+    # 上传文件
+    def put(self, filename):
+        try:
+            f = open(filename,'rb')
+        except:
+            print("文件不存在")
+            return
+        data = "P " + filename
+        self.sockfd.send(data.encode())  # 发送请求
+        # 等待回复
+        data = self.sockfd.recv(128).decode()
+        if data == 'OK':
+            while True:
+                data = f.read(1024)
+                if not data:
+                    time.sleep(0.1)
+                    self.sockfd.send(b'##')  # 结束标志
+                    break
+                self.sockfd.send(data)
+            f.close()
+        else:
+            print(data)
 
 # 启动函数
 def main():
@@ -53,6 +96,12 @@ def main():
             ftp.quit()
         elif cmd == 'list':
             ftp.list()
+        elif cmd[:3] == 'get':
+            filename = cmd.split(' ')[-1]
+            ftp.get(filename)
+        elif cmd[:3] == 'put':
+            filename = cmd.split(' ')[-1]
+            ftp.put(filename)
         else:
             print("请输入正确命令")
 
--- day10/ftp_server.py
+++ day10/ftp_server.py
@@ -10,7 +10,7 @@ import os,time
 
 # 全局变量
 HOST = '0.0.0.0'
-PORT = 8888
+PORT = 8800
 ADDR = (HOST,PORT)
 
 # 文件库
@@ -38,6 +38,44 @@ class FTPServer(Thread):
         data = '\n'.join(file_list)  # 将文件以\n拼接
         self.connfd.send(data.encode())
 
+    # 处理下载文件
+    def get(self,filename):
+        try:
+            f = open(FTP + filename,'rb')
+        except:
+            # 不存在这个文件
+            self.connfd.send("文件不存在".encode())
+            return
+        else:
+            self.connfd.send(b'OK')
+            time.sleep(0.1)
+        # 发送文件
+        while True:
+            data = f.read(1024)
+            if not data:
+                time.sleep(0.1)
+                self.connfd.send(b'##') # 结束标志
+                break
+            self.connfd.send(data)
+        f.close()
+
+    # 处理上传
+    def put(self,filename):
+        if os.path.exists(FTP+filename):
+            self.connfd.send("文件已存在".encode())
+            return
+        else:
+            self.connfd.send(b'OK')
+        # 接收文件
+        # 循环接收文件写入本地
+        f = open(FTP+filename,'wb')
+        while True:
+            data = self.connfd.recv(1024)
+            # 文件接收完毕的标志
+            if data == b'##':
+                break
+            f.write(data)
+        f.close()
 
     #  循环接收请求，分发任务
     def run(self):
@@ -47,6 +85,13 @@ class FTPServer(Thread):
                 return   # run函数结束对应线程即退出
             elif data == 'L':
                 self.list()
+            elif data[0] == 'G':
+                filename = data.split(' ')[-1]
+                self.get(filename)
+            elif data[0] == 'P':
+                filename = data.split(' ')[-1]
+                self.put(filename)
+
 
 # 框架结构，启动函数
 def main():
