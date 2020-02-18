0248cc6a9ed957592da766086a1267e3febec67e
Levi <lvze@tedu.cn>
2020-02-18 17:19:41 +0800

    d11

--- day11/http_server.py
+++ day11/http_server.py
@@ -13,16 +13,64 @@ class HTTPServer:
         self.port = port
         self.address = (host,port)
         self.dir = dir
+        # 多路服用监控列表
+        self.rlist = []
+        self.wlist = []
+        self.xlist = []
         self.sockfd = socket()  # 套接字属性
         self.sockfd.setblocking(False)
         self.sockfd.bind(self.address)
-
 
     # 启动服务
     def serve_forever(self):
         self.sockfd.listen(3)
         print("Listen the port %d"%self.port)
         # 使用IO多路服用处理客户端请求
+        self.rlist.append(self.sockfd)
+        while True:
+            rs,ws,xs = select(self.rlist,
+                              self.wlist,
+                              self.xlist)
+            for r in rs:
+                if r is self.sockfd:
+                    c,addr = r.accept()
+                    c.setblocking(False)
+                    self.rlist.append(c) # 增加监控对象
+                else:
+                    # 浏览器发送http请求
+                    self.handle(r)
+
+    # 处理客户端具体请求
+    def handle(self,connfd):
+        request = connfd.recv(4096) # 接收http请求
+
+        # 防止客户端退出data等于空
+        if not request:
+            self.rlist.remove(connfd) # 移除监控
+            connfd.close()
+            return
+
+        info = request.decode().split(' ')[1]
+        print("请求内容：", info)
+
+        # 请求主页
+        if info == '/':
+            info = '/index.html'
+
+        try:
+            f = open(self.dir+info)
+        except:
+            data = "HTTP/1.1 404 Not Found\r\n"
+            data += "Content-Type:text/html\r\n"
+            data += "\r\n"
+            data += "Sorry...."
+        else:
+            data = "HTTP/1.1 200 OK\r\n"
+            data += "Content-Type:text/html\r\n"
+            data += "\r\n"
+            data += f.read() # 网页内容
+            f.close()
+        connfd.send(data.encode())
 
 
 
