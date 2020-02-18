06c8a122e52466d62480f0d489de6acd5c01ddb4
Levi <lvze@tedu.cn>
2020-02-18 10:42:11 +0800

    d11

--- day11/select _server.py
+++ day11/select _server.py
@@ -17,6 +17,8 @@ s = socket()
 s.bind(('0.0.0.0',8888))
 s.listen(3)
 
+s.setblocking(False)
+
 # 设置关注列表
 rlist = [s]  # 处理客户端链接
 wlist = []
@@ -31,17 +33,23 @@ while True:
         if r is s:
             c, addr = r.accept()
             print("Connect from",addr)
+            c.setblocking(False)  # 设置非阻塞
             rlist.append(c) # 将对应的客户端链接套接字加入列表
         else:
             # 如果r遍历到客户端链接套接字说明： 有客户端给我发消息
             data = r.recv(1024)
             if not data:
-                break
+                # 客户端退出
+                rlist.remove(r) # 将其从关注的Io 中移除
+                r.close()
+                continue
             print(data.decode())
-            r.send(b'OK')
+            # r.send(b'OK')
+            wlist.append(r)  # 将要主动发送消息的IO对象加入写关注
 
     for w in ws:
-        pass
+        w.send(b'OK')
+        wlist.remove(w)
 
     for x in xs:
         pass
