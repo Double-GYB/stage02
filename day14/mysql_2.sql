b6faf7a97e41d40edb2fde3579df81c02fd852f7
Levi <lvze@tedu.cn>
2020-02-22 12:03:08 +0800

    d14

--- day14/mysql_2.sql
+++ day14/mysql_2.sql
@@ -27,13 +27,31 @@ select * from class_1  where sex is null;
 -- 练习2 查找练习
 --
 -- 1. 查找30多图书
+      select * from book where price >= 30 and price < 40;
 -- 2. 查找中国教育出版社出版的图书
+      select * from book where publication = "中国教育出版社";
 -- 3. 查找老舍写的，中国文学出版社出版的
+      select * from book where author = '老舍' and publication="中国文学出版社";
 -- 4. 查找备注不为空的
+      select * from book comment is not null;
 -- 5. 查找价格查过60的，只看书名和价格
+      select title,price from book where price > 60;
 -- 7. 查找鲁迅写的 或者 矛盾写的
+      select * from book where author ='鲁迅' or author ='矛盾';
+      select * from book where author in ('鲁迅','矛盾');
 
 
+-- alter 操作
+alter table interest add tel char(11) after price;
+alter table interest drop level;
+alter table interest modify tel  char(16);
+alter table interest change tel phone char(16);
+alter table class_1 rename cls;
+
+时间类型
+select * from marathon where birthday > '1990-01-01';
+select * from marathon where registration_time < now();
+select * from marathon where birthday > '1990-01-01';
 
 
 
