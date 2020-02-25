16ce1590c4df899ea8ebd58d414b4a863d7a4e1a
Levi <lvze@tedu.cn>
2020-02-25 09:46:03 +0800

    d15

---
+++ day16/mysql_4.sql
@@ -0,0 +1,39 @@
+-- E-R模型中学生  教师   课程表的建立
+create table stu(
+id int primary key auto_increment,
+姓名 varchar(30),
+年龄 tinyint,
+性别 char,
+籍贯 varchar(128)
+);
+
+create table teacher(
+id int primary key auto_increment,
+姓名 varchar(30),
+职称 varchar(30),
+年龄 tinyint
+);
+
+create table course(
+id int primary key auto_increment,
+名称 varchar(30),
+学分 float,
+tid int,
+constraint  t_fk
+foreign  key (tid)
+references teacher(id)
+);
+
+create table course_stu(
+cid int,
+sid int,
+score float,
+primary key(cid,sid),
+constraint  c_fk
+foreign  key (cid)
+references course(id),
+constraint  s_fk
+foreign  key (sid)
+references stu(id)
+);
+
