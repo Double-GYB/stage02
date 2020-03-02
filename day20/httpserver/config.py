13d15a2d22768deb36639a94c2e49eaebb9a28e1
Tedu <lvze@tedu.cn>
2020-03-02 14:56:05 +0800

    d20

--- day20/httpserver/config.py
+++ day20/httpserver/config.py
@@ -8,3 +8,7 @@ PORT = 8000
 
 # [DEBUG]
 DEBUG = True
+
+# 访问webframe需要的地址
+frame_ip = '127.0.0.1'
+frame_port = 8080
--- day20/httpserver/httpserver.py
+++ day20/httpserver/httpserver.py
@@ -12,12 +12,30 @@ from socket import *
 import sys
 from threading import Thread
 from config import *  # 导入用户配置
+import re,json
 
 # 服务器地址
 ADDR = (HOST, PORT)
 
+# 处理和webframe交互
+def connect_frame(env):
+    # 创建tcp客户端
+    s = socket()
+    s.connect((frame_ip,frame_port))
 
-# 主体功能
+    try:
+        # 将请求字典转换为json格式发送
+        data = json.dumps(env)
+        s.send(env.encode())
+        # 等待数据
+        data = s.recv(1024 * 1024 * 10).decode()
+        return json.loads(data) # 转换为字典返回
+    except:
+        return
+
+
+
+# 主体功能 (处理与浏览器交互，负责http协议部分操作)
 class HTTPServer:
     def __init__(self):
         self.address = ADDR
@@ -26,7 +44,7 @@ class HTTPServer:
 
     # 创建套接字
     def create_socket(self):
-        # tcp套接字
+        # tcp套接字 与浏览器通信
         self.sockfd = socket()
         self.sockfd.setsockopt(SOL_SOCKET, SO_REUSEADDR, DEBUG)
 
@@ -48,9 +66,34 @@ class HTTPServer:
 
     # 具体处理浏览器请求
     def handle(self,connfd):
-        request = connfd.recv(4096)  # 接收http请求
-        print(request)
+        request = connfd.recv(4096).decode()  # 接收http请求
+        pattern = r"(?P<method>[A-Z]+)\s+(?P<info>/\S*)"
+        try:
+            env = re.match(pattern,request).groupdict()
+        except:
+            connfd.close()
+            return
+        else:
+            data = connect_frame(env)  # 交给 frame 交互函数处理
+            if data:
+                # data ==> {'status':'200','data':'xxxxx'}
+                self.response(connfd,data)
 
+    # 组织给浏览器的响应格式
+    def response(self,connfd,data):
+        if data['status']  == '200':
+            res = "HTTP/1.1 200 OK\r\n"
+            res += "Content-Type:text/html\r\n"
+            res += "\r\n"
+            res += data['data']
+        elif data['status']  == '404':
+            res = "HTTP/1.1 404 Not Found\r\n"
+            res += "Content-Type:text/html\r\n"
+            res += "\r\n"
+            res += data['data']
+        elif data['status']  == '500':
+            pass
+        connfd.send(res.encode()) # 发送给浏览器
 
 httpd = HTTPServer()
 httpd.serve_forever()  # 启动服务
